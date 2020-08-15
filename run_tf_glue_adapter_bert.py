d632369a79e039fd159467cf32878d5fbe6af967
Hosein <hosein.mohebbi75@gmail.com>
2020-08-15 05:30:21 +0430

    Fixed evaluation

--- run_tf_glue_adapter_bert.py
+++ run_tf_glue_adapter_bert.py
@@ -1,9 +1,10 @@
-import modeling
-import utility
+import modeling_tf_adapter_bert
 import os
 import tensorflow as tf
 import tensorflow_datasets
 import numpy as np
+from scipy.stats import spearmanr
+from sklearn.metrics import matthews_corrcoef, f1_score, accuracy_score
 from transformers import (
     BertConfig,
     BertTokenizer,
@@ -30,7 +31,7 @@ def main():
 
   parser.add_argument('--casing', type=str, default="bert-base-uncased", help='BERT model')
   parser.add_argument('--bottleneck_size', type=int, default=64, help='Bottleneck size of adapters')
-  parser.add_argument('--non_linearity', type=str, default='gelu', help='non_linearity function in adapters')
+  parser.add_argument('--non_linearity', type=str, default='gelu_new', help='non_linearity function in adapters')
   parser.add_argument('--task', type=str, default='mrpc', help='GLUE task')
   parser.add_argument('--batch_size', type=int, default=32, help='batch size')
   parser.add_argument('--epochs', type=int, default=10, help='The number of training epochs')
@@ -71,32 +72,34 @@ def main():
 
   tokenizer = BertTokenizer.from_pretrained(args.casing)
   bert_model = TFBertModel(config).from_pretrained(args.casing)
-  model = modeling.AdapterBertModel(bert_model, num_labels)
+  model = modeling_tf_adapter_bert.AdapterBertModel(bert_model, num_labels)
 
   data, info = tensorflow_datasets.load(TFDS_TASK, with_info=True)
   train_examples = info.splits["train"].num_examples
-  valid_examples = info.splits["validation"+postfix].num_examples
+  eval_examples = info.splits["validation"+postfix].num_examples
 
   train_dataset = glue_convert_examples_to_features(data["train"], tokenizer, max_length=args.max_seq_length, task=args.task)
-  valid_dataset = glue_convert_examples_to_features(data["validation"+postfix], tokenizer, max_length=args.max_seq_length, task=args.task)
+  eval_dataset = glue_convert_examples_to_features(data["validation"+postfix], tokenizer, max_length=args.max_seq_length, task=args.task)
     
   train_dataset = train_dataset.repeat().shuffle(buffer_size=100).batch(args.batch_size)
-  valid_dataset = valid_dataset.batch(args.batch_size)
+  eval_dataset = eval_dataset.batch(args.batch_size)
 
   train_steps = int(np.ceil(train_examples / args.batch_size))
-  valid_steps = int(np.ceil(valid_examples / args.batch_size))
+  eval_steps = int(np.ceil(eval_examples / args.batch_size))
 
 
   # Add Adapters
   for i in range(config.num_hidden_layers):
     # instantiate
-    model.bert.bert.encoder.layer[i] = modeling.TFBertLayer(model.bert.bert.encoder.layer[i].attention.self_attention,
+    model.bert.bert.encoder.layer[i].attention.dense_output = modeling_tf_adapter_bert.TFBertSelfOutput(
                                                   model.bert.bert.encoder.layer[i].attention.dense_output.dense,
                                                   model.bert.bert.encoder.layer[i].attention.dense_output.LayerNorm,
-                                                  model.bert.bert.encoder.layer[i].intermediate,
-                                                  model.bert.bert.encoder.layer[i].bert_output.dense,
-                                                  model.bert.bert.encoder.layer[i].bert_output.LayerNorm,
                                                   config)
+    model.bert.bert.encoder.layer[i].bert_output = modeling_tf_adapter_bert.TFBertOutput(
+                                                  model.bert.bert.encoder.layer[i].bert_output.dense,
+                                                  model.bert.bert.encoder.layer[i].bert_output.LayerNorm, 
+                                                  config)
+  
     
   # Freeze BERT
   model.bert.bert.embeddings.trainable = False
@@ -110,22 +113,18 @@ def main():
     model.bert.bert.encoder.layer[i].bert_output.LayerNorm.trainable = False
 
 
-  # Metrics, Loss & Optimizer
+  # Loss & Optimizer
   if num_labels == 1:
       loss = tf.keras.losses.MeanSquaredError()
-      metric = utility.spearman
-      monitor = "val_spearman"
+      monitor = 'val_spearmanr'
   else:
       loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
       if args.task == 'cola':
-        metric = utility.matthews_cc
-        monitor = "val_matthews_cc"
+        monitor = 'val_matthews_corrcoef'
       elif args.task in ['mrpc', 'qqp']:
-        metric = utility.f1
-        monitor = "val_f1"
+        monitor = 'val_f1'
       else:
-        metric = tf.keras.metrics.SparseCategoricalAccuracy("accuracy")
-        monitor = "val_accuracy"
+        monitor = 'val_accuracy'
                 
 
   opt, scheduler = create_optimizer(init_lr=args.learning_rate,
@@ -133,22 +132,66 @@ def main():
                         num_warmup_steps=int(train_steps * args.epochs * args.warmup_ratio),
                         adam_epsilon=1e-6,
                         weight_decay_rate = 0)
-  model.compile(optimizer=opt, loss=loss, metrics=metric)
+  model.compile(optimizer=opt, loss=loss)
 
-  # Callback
-  checkpoint = utility.ModelCheckpoint(monitor, os.path.join(args.saved_models_dir, args.task))
+  # Callback to save the best model
+  checkpoint = ModelCheckpoint(eval_dataset, args.batch_size, eval_steps, monitor, os.path.join(args.saved_models_dir, args.task))
 
   # Fine-tuning
   history = model.fit(
       train_dataset,
       epochs=args.epochs,
       steps_per_epoch=train_steps,
-      validation_data=valid_dataset,
-      validation_steps=valid_steps,
       callbacks=[checkpoint]
   )
 
 
+class ModelCheckpoint(tf.keras.callbacks.Callback):
+  def __init__(self, eval_dataset, eval_batch_size, eval_steps, monitor, save_path):
+    super(ModelCheckpoint, self).__init__()
+    self.eval_dataset = eval_dataset
+    self.eval_batch_size = eval_batch_size
+    self.eval_steps = eval_steps
+    self.monitor = monitor
+    self.save_path = save_path
+    self.best_val_score = -np.Inf
+
+    self.val_labels: np.ndarray = None
+    for batch in self.eval_dataset:
+      if self.val_labels is None:
+        self.val_labels = batch[1].numpy()
+      else:
+        self.val_labels = np.append(self.val_labels, batch[1].numpy(), axis=0)
+
+  def evaluate(self):
+    val_preds = self.model.predict(self.eval_dataset, batch_size=self.eval_batch_size, steps=self.eval_steps)
+
+    if self.monitor == 'val_spearmanr':
+      val_preds = tf.clip_by_value(val_preds, clip_value_min=0.0, clip_value_max=5.0)
+      return spearmanr(self.val_labels, val_preds)[0]
+
+    elif self.monitor == 'val_matthews_corrcoef':
+      val_preds = tf.math.argmax(val_preds, axis=1)
+      return matthews_corrcoef(self.val_labels, val_preds)
+
+    elif self.monitor == 'val_f1':
+      val_preds = tf.math.argmax(val_preds, axis=1)
+      return f1_score(self.val_labels, val_preds)
+
+    else:
+      val_preds = tf.math.argmax(val_preds, axis=1)
+      return accuracy_score(self.val_labels, val_preds)
+
+  def on_epoch_end(self, epoch, logs):
+    val_score = self.evaluate()
+    print(" - {0}: {1:.4f}".format(self.monitor, val_score))
+
+    if val_score >= self.best_val_score:
+      path = os.path.join(self.save_path, str(epoch+1))
+      os.makedirs(path)
+      self.model.save_weights(path+'/best_weights.h5')
+      self.best_val_score = val_score
+      print("Model saved in epoch {} as the best model".format(epoch+1))
 
 if __name__ == "__main__":
     main()
